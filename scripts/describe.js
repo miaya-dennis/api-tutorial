1fe4672fa5f6a8570f4f1fce3d521d83b4741076
Miaya <myfirefly22@gmail.com>
2026-08-30 23:01:36 -0400

    added files for day 3

--- scripts/describe.js
+++ scripts/describe.js
@@ -1,23 +1,3 @@
 import pets from "../data.json";
-console.log("Hello World");
 
-console.log(pets[0];
-	
-	
-import pets from "../data.json";
-
-function sayLength(size) {
-  console.log("This cat measures " + size + ".");
-}
-
-function describe(pet) {
-  console.log(pet.name + " comes from " + pet.origin + ".");
-}
-
-sayLength("12 to 16 inches");
-sayLength("15 to 20 inches");
-sayLength("30 to 40 inches");
-
-describe(pets[0]);
-describe(pets[1]);
-describe(pets[2]);
+console.log(pets[0]);
