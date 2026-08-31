474d394df9ad34db154be02e06d375dd0c790948
Miaya <myfirefly22@gmail.com>
2026-08-30 23:15:42 -0400

    changed heading

--- scripts/describe.js
+++ scripts/describe.js
@@ -1,3 +1,18 @@
 import pets from "../data.json";
 
-console.log(pets[0]);
+
+function sayLength(size) {
+  console.log("This cat measures " + size + ".");
+}
+
+function describe(pet) {
+  console.log(pet.name + " comes from " + pet.origin + ".");
+}
+
+sayLength("12 to 16 inches");
+sayLength("15 to 20 inches");
+sayLength("30 to 40 inches");
+
+describe(pets[0]);
+describe(pets[1]);
+describe(pets[2]);
